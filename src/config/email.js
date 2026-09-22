fcf3bd88af985751b4d4094bba8053cc65683bc3
BRANDON FELIPE MORALES HERRERA <adm-felipem@sya.local>
2026-09-22 10:52:23 -0500

    jijji

--- src/config/email.js
+++ src/config/email.js
@@ -1,8 +1,8 @@
 export const EMAILJS_SERVICE_ID =
-  import.meta.env.VITE_EMAILJS_SERVICE_ID || "service_jx0zh15";
+  import.meta.env.VITE_EMAILJS_SERVICE_ID || "service_2kgkgje";
 
 export const EMAILJS_TEMPLATE_ID =
-  import.meta.env.VITE_EMAILJS_TEMPLATE_ID || "template_c29iebm";
+  import.meta.env.VITE_EMAILJS_TEMPLATE_ID || "template_224v0rm";
 
 export const EMAILJS_PUBLIC_KEY =
-  import.meta.env.VITE_EMAILJS_PUBLIC_KEY || "WB-xnoNbKku7X_qgL";
+  import.meta.env.VITE_EMAILJS_PUBLIC_KEY || "4t15HIFT8jhZUaRBl";
